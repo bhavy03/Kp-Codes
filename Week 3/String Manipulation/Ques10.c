95845cdd1fb751b58a39c40d03b83af83777a6f6
Bhavya <bhavya7664@gmail.com>
2025-01-12 20:32:25 +0530

    13

--- Week 3/String Manipulation/Ques10.c
+++ Week 3/String Manipulation/Ques10.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 int strLen(char *str)
 {
     int count = 0;
@@ -7,6 +8,18 @@ int strLen(char *str)
         count++;
     }
     return count;
+}
+char *findSubstring(char *str, int start, int end)
+{
+    char *subString = (char *)malloc(sizeof(char) * (end - start + 2));
+    // char subString[end - start + 1]; this thing can only be used within the function
+    int index = 0;
+    for (int i = start; i <= end; i++)
+    {
+        subString[index++] = str[i];
+    }
+    subString[index] = '\0';
+    return subString;
 }
 int isPalindrome(char str[])
 {
@@ -38,10 +51,98 @@ int main()
     // printf("Not Palindrome\n");
     // }
     int len = strLen(input);
-    for(int i =0;input[i]!='\0';i++)
+    // char *subString = findSubstring(input, 2, 6);
+    // printf("%s", subString);
+
+    int maxLength = 0;
+    int startIdx = 0;
+    if (len == 1)
     {
-        char *str = input[i];
-        for(int j = i+1;j<len;j++){}
+        printf("%s", input);
+        return 0;
     }
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            char *temp = findSubstring(input, i, j);
+            if (isPalindrome(temp))
+            {
+                int currentLen = j - i + 1;
+                if (currentLen > maxLength)
+                {
+                    maxLength = currentLen;
+                    startIdx = i;
+                }
+            }
+            free(temp);
+        }
+    }
+    char *longestPalindrome = findSubstring(input, startIdx, startIdx + maxLength - 1);
+    printf("%s\n", longestPalindrome);
+    free(longestPalindrome);
+
     return 0;
 }
+// #include <stdio.h>
+// #include <string.h>
+//
+// char* longestPalindrome(char* s);
+// char* expandFromCenter(char* s, int left, int right, char* result);
+//
+// int main() {
+// char input[200];
+// printf("Enter the string: ");
+// scanf("%s", input);
+//
+// char* longest = longestPalindrome(input);
+// printf("Longest Palindromic Substring: %s\n", longest);
+//
+// return 0;
+// }
+//
+// char* longestPalindrome(char* s) {
+// static char longest[200] = ""; // Static to persist data outside the function
+// longest[0] = '\0'; // Reset the string
+//
+// int n = strlen(s);
+//
+// for (int i = 0; i < n; i++) {
+// char temp[200];
+//
+// Odd - length palindromes
+// expandFromCenter(s, i, i, temp);
+// if (strlen(temp) > strlen(longest)) {
+// strcpy(longest, temp);
+// }
+//
+//   Even -
+// length palindromes
+// expandFromCenter(s, i, i + 1, temp);
+// if (strlen(temp) > strlen(longest)) {
+// strcpy(longest, temp);
+// }
+// }
+//
+// return longest;
+// }
+//
+// char* expandFromCenter(char* s, int left, int right, char* result) {
+// int n = strlen(s);
+//
+// while (left >= 0 && right < n && s[left] == s[right]) {
+// left--;
+// right++;
+// }
+//
+// Extract the substring from left +
+// 1 to right - 1
+// int start = left + 1;
+// int length = right - left - 1;
+//
+// strncpy(result, &s[start], length);
+// result[length] = '\0'; // Null-terminate the string
+//
+// return result;
+// }
+//
---
+++ Week 3/String Manipulation/Ques13.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+int strLen(char *str)
+{
+    int count = 0;
+    while (str[count] != '\0')
+    {
+        count++;
+    }
+    return count;
+}
+char *findSubstring(char *str, int start, int end)
+{
+    char *subString = (char *)malloc(sizeof(char) * (end - start + 2));
+    int index = 0;
+    for (int i = start; i <= end; i++)
+    {
+        subString[index++] = str[i];
+    }
+    subString[index] = '\0';
+    return subString;
+}
+int ifContainAll(char *input1, char *input2)
+{
+    int len1 = strLen(input1);
+    int len2 = strLen(input2);
+    int count = 0;
+    for (int i = 0; i < len1; i++)
+    {
+        for (int j = 0; j < len2; j++)
+        {
+            if (input2[i] == input1[j])
+            {
+                count++;
+            }
+        }
+    }
+    if (count == len2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    char input1[200];
+    printf("Enter the first String\n");
+    scanf("%[^\n]%*c", input1);
+    int len1 = strLen(input1);
+
+    char input2[200];
+    printf("Enter the senond String\n");
+    scanf("%[^\n]%*c", input2);
+    int len2 = strLen(input2);
+
+    int minLength = len1;
+    int startIdx = 0;
+
+    for (int i = 0; input1[i] != '\0'; i++)
+    {
+        for (int j = i + 1; j < len1; j++)
+        {
+            char *temp = findSubstring(input1, i, j);
+            if (ifContainAll(temp, input2))
+            {
+                int currentLen = j - i + 1;
+                if (currentLen < minLength)
+                {
+                    minLength = currentLen;
+                    startIdx = i;
+                }
+            }
+            free(temp);
+        }
+    }
+    char *longestPalindrome = findSubstring(input1, startIdx, startIdx + minLength - 1);
+    printf("%s\n", longestPalindrome);
+    free(longestPalindrome);
+
+    return 0;
+}
+// int ifContainAll(char *input1, char *input2)
+// {
+// int hash1[256] = {0}, hash2[256] = {0};
+// int len1 = strLen(input1);
+// int len2 = strLen(input2);
+//
+// Build frequency map for input2
+// for (int i = 0; i < len2; i++)
+// {
+// hash2[input2[i]]++;
+// }
+//
+// Traverse input1 and count matches
+// for (int i = 0; i < len1; i++)
+// {
+// hash1[input1[i]]++;
+// }
+//
+// Check if all characters in hash2 are covered by hash1
+// for (int i = 0; i < 256; i++)
+// {
+// if (hash2[i] > hash1[i])
+// {
+// return 0;
+// }
+// }
+// return 1;
+// }

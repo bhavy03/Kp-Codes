1b61a8735099e93e39997435044ef8c6efaf3890
Bhavya Soni <99943536+bhavy03@users.noreply.github.com>
2025-01-21 10:23:04 +0530

    Create Correct1.c

---
+++ Assessment/Correct1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void invert(int n, int arr[n][n], int r, int c) {
+  if (arr[r][c] == 0) {
+    arr[r][c] = 1;
+  } else {
+    arr[r][c] = 0;
+  }
+  printf("invert called\n");
+}
+void flip(int n, int arr[n][n]) {
+  int start = 1;
+  int end = n;
+  while (start < end) {
+    for (int i = 1; i <= n; i++) {
+      int temp = arr[start][i];
+      arr[start][i] = arr[end][i];
+      arr[end][i] = temp;
+    }
+    start++;
+    end--;
+  }
+  printf("flip called\n");
+}
+void rotate(int n, int arr[n][n]) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = i + 1; j <= n; j++) {
+      int temp = arr[i][j];
+      arr[i][j] = arr[j][i];
+      arr[j][i] = temp;
+    }
+  }
+  flip(n, arr);
+}
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  // int **arr = (int **)malloc((n) * sizeof(int *));
+  // for(int i=0; i<n; i++){
+  //   arr[i] = (int *)malloc((n) * sizeof(int));
+  // }
+  int arr[n][n];
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      arr[i][j] = 0;
+    }
+  }
+
+  int m;
+  scanf("%d", &m);
+
+  while (m > 0) {
+    int k;
+    scanf("%d", &k);
+    if (k == 1) {
+      int r, c;
+      scanf("%d %d", &r, &c);
+      invert(n, arr, r, c);
+    }
+    if (k == 2) {
+      flip(n, arr);
+    }
+    if (k == 3) {
+      rotate(n, arr);
+    }
+    m--;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      printf("%d ", arr[i][j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}

1b38561f5ed63d1ccf19e9d190062d56a4bf525d
Bhavya Soni <99943536+bhavy03@users.noreply.github.com>
2025-01-24 15:59:23 +0530

    Update Cycle.c

--- Week 4/Cycle Detection/Cycle.c
+++ Week 4/Cycle Detection/Cycle.c
@@ -9,5 +9,30 @@ bool has_cycle(SinglyLinkedListNode* head) {
         }
     }
     return false;
+}
 
+// this is to detect the starting point of loop
+struct Node *detectCycle(struct Node *head) {
+    struct Node *slow = head;
+    struct Node *fast = head;
+    if(head == NULL || head->next == NULL){
+        return NULL;
+    }
+    
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) { 
+            
+            slow = head; 
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow; 
+        }
+    }
+
+    return NULL;
 }

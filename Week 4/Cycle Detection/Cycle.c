24db613262b4fbeef990734a803880e5865f3855
Bhavya Soni <99943536+bhavy03@users.noreply.github.com>
2025-01-24 08:44:20 +0530

    Create Cycle.c

---
+++ Week 4/Cycle Detection/Cycle.c
@@ -0,0 +1,13 @@
+bool has_cycle(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* slow = head;
+    SinglyLinkedListNode* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            return true;
+        }
+    }
+    return false;
+
+}
